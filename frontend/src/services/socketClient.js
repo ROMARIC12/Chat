f0a8f9cc140b8c681bb039986d491cb04305ffae
ROMARIC12 <110196501+ROMARIC12@users.noreply.github.com>
2025-09-21 18:47:39 +0000

    Update socketClient.js

--- frontend/src/services/socketClient.js
+++ frontend/src/services/socketClient.js
@@ -1,7 +1,7 @@
 import { io } from "socket.io-client";
 
 const API_URL =
-  import.meta.env.VITE_API_URL";
+  import.meta.env.VITE_API_URL;
 const SOCKET_URL = API_URL.replace(/\/api$/, ''); // Enlève /api si présent
 
 const socket = io(SOCKET_URL, {
